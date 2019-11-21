88ad20e6828326fe11413f62734251f0da3d2e10
Sonja Cao <csonja1703@gmail.com>
2019-11-21 16:26:28 +0100

    Added method for creating nodes from Json file

--- indoor-navigation-app/src/main/java/at/htl/indoornav/control/InitBean.java
+++ indoor-navigation-app/src/main/java/at/htl/indoornav/control/InitBean.java
@@ -5,8 +5,14 @@ import at.htl.indoornav.repository.DatabaseRespository;
 import io.quarkus.runtime.StartupEvent;
 import org.neo4j.ogm.cypher.ComparisonOperator;
 import org.neo4j.ogm.cypher.Filter;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.InputStreamReader;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -19,14 +25,26 @@ public class InitBean {
      * @param event
      */
     void init(@Observes StartupEvent event) {
-        MapNode mapNode = new MapNode("Lift", "Lift OG", "1 OG", 149.0, 200.0);
+//        MapNode mapNode = new MapNode("Lift", "Lift OG", "1 OG", 149.0, 200.0);
+        JsonArray jsonArray = readJsonFromFile("data.json");
 
-        if (!nodeExists(mapNode)) {
-            DatabaseRespository.getINSTANCE().getSession().save(mapNode);
-            System.out.println(mapNode + " created");
-        } else {
-            System.out.println("Node already exists");
-        }
+        jsonArray.forEach(jsonValue -> {
+            JsonObject jsonObject = jsonValue.asJsonObject();
+
+            MapNode mapNode = new MapNode();
+            mapNode.setType(jsonObject.getString("type"));
+            mapNode.setName(jsonObject.getString("name"));
+            mapNode.setFloor(jsonObject.getString("floor"));
+            mapNode.setxCoordinate(Double.valueOf(jsonObject.getJsonNumber("xCoordinate").toString()));
+            mapNode.setyCoordinate(Double.valueOf(jsonObject.getJsonNumber("yCoordinate").toString()));
+
+            if (!nodeExists(mapNode)) {
+                DatabaseRespository.getINSTANCE().getSession().save(mapNode);
+                System.out.println(mapNode + " created");
+            } else {
+                System.out.println("Node already exists");
+            }
+        });
     }
 
     /**
@@ -48,5 +66,19 @@ public class InitBean {
             }
         }
         return false;
+    }
+
+    /**
+     * Read Json Array from file
+     *
+     * @param filename name of the file
+     * @return Json Array with data
+     */
+    private JsonArray readJsonFromFile(String filename) {
+        try (JsonReader jsonReader = Json.createReader(
+                new InputStreamReader(getClass().getResourceAsStream("/" + filename))
+        )) {
+            return jsonReader.readArray();
+        }
     }
 }
