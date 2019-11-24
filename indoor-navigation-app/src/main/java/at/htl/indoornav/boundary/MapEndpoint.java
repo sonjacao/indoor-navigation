1785288b88b654edf19ab00fec9cf5d9193b08fc
Sonja Cao <csonja1703@gmail.com>
2019-11-24 17:40:17 +0100

    Created REST Endpoint for map nodes

---
+++ indoor-navigation-app/src/main/java/at/htl/indoornav/boundary/MapEndpoint.java
@@ -0,0 +1,11 @@
+package at.htl.indoornav.boundary;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("map")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class MapEndpoint {
+
+}
