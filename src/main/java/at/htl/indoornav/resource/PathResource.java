d24c73a80282b3ce860145087a60506a94bda17a
Halil Bahar <halil.s.bahar@gmail.com>
2020-01-19 14:23:57 +0100

    Add prefix to the paths in the PathResource endpoint

--- src/main/java/at/htl/indoornav/resource/PathResource.java
+++ src/main/java/at/htl/indoornav/resource/PathResource.java
@@ -9,14 +9,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("path")
+@Path("/path")
 public class PathResource {
 
     @Inject
     NodeRepository nodeRepository;
 
     @GET
-    @Path("shortest")
+    @Path("/shortest")
     public Response getShortestPath(@QueryParam("start") Long idStart, @QueryParam("end") Long idEnd) {
         Node startNode = nodeRepository.getNodeById(idStart);
         Node endNode = nodeRepository.getNodeById(idEnd);
@@ -25,7 +25,7 @@ public class PathResource {
     }
 
     @GET
-    @Path("handicapped")
+    @Path("/handicapped")
     public Response getShortestPathForHandicapped(@QueryParam("start") Long idStart, @QueryParam("end") Long idEnd) {
         Node startNode = nodeRepository.getNodeById(idStart);
         Node endNode = nodeRepository.getNodeById(idEnd);
