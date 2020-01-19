8e38d822d46cace010e2997e573a986f15c532bd
sonjacao <csonja1703@gmail.com>
2020-01-19 15:01:47 +0100

    Create test for NodeEndpoint

---
+++ src/test/java/at/htl/indoornav/NodeResourceTest.java
@@ -0,0 +1,41 @@
+package at.htl.indoornav;
+
+import at.htl.indoornav.entity.Node;
+import at.htl.indoornav.entity.NodeType;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.*;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class NodeResourceTest {
+
+    Jsonb jsonb;
+
+    @BeforeAll
+    void init() {
+        jsonb = JsonbBuilder.create();
+    }
+
+    @Test
+    void testGetAllNodes() {
+        given()
+                .when().get("/node")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void testCreateNode() {
+        Node node = new Node(null, "5ahif", NodeType.FLOOR, false, 125f, 25f, 890f);
+        given()
+                .when().contentType("application/json").body(jsonb.toJson(node)).post("/node")
+                .then()
+                .statusCode(200);
+    }
+
+}
