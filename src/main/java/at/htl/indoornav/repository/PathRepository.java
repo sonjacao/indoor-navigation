41f0140657ab5144d80608cb876349a17fb4b259
Halil Bahar <halil.s.bahar@gmail.com>
2020-01-28 20:05:11 +0100

    Simplify code for shortest path

--- src/main/java/at/htl/indoornav/repository/PathRepository.java
+++ src/main/java/at/htl/indoornav/repository/PathRepository.java
@@ -3,15 +3,12 @@ package at.htl.indoornav.repository;
 import at.htl.indoornav.entity.Node;
 import at.htl.indoornav.entity.NodeType;
 import org.neo4j.driver.Record;
-import org.neo4j.driver.StatementResult;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ApplicationScoped
@@ -35,19 +32,17 @@ public class PathRepository extends BaseRepository {
         parameters.put("type", forHandicapped ? NodeType.STAIRS.name() : "null");
         // TODO: 20.01.20 handicapped handling
 
-        StatementResult result = driver.session().run(
-                "MATCH (start:Point), (end:Point) WHERE start.name = $start AND end.name = $end " +
-                        "CALL algo.shortestPath.stream(start, end, 'distance') " +
-                        "YIELD nodeId, cost RETURN algo.asNode(nodeId).name AS name, cost",
-                parameters
-        );
+        String query = "MATCH (start:Point), (end:Point) WHERE start.name = $start AND end.name = $end " +
+                "CALL algo.shortestPath.stream(start, end, 'distance') " +
+                "YIELD nodeId, cost RETURN algo.asNode(nodeId).name AS name, cost";
+
+        List<Record> records = executeQueryList(query, parameters);
 
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-        while (result.hasNext()) {
-            Record next = result.next();
+        for (Record record : records) {
             // Exception for asFloat
-            float cost = (float) next.get("cost").asDouble();
-            String nodeName = next.get("name").asString();
+            float cost = (float) record.get("cost").asDouble();
+            String nodeName = record.get("name").asString();
             Node node = nodeRepository.getNode(nodeName);
             JsonObjectBuilder jsonNode = Json.createObjectBuilder()
                     .add("id", node.getId())
