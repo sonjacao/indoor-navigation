7ee2f847f526428eae5a1c10bc8a0f813d7404c1
Halil Bahar <halil.s.bahar@gmail.com>
2020-01-28 20:13:12 +0100

    Improve relationship validation

--- src/main/java/at/htl/indoornav/resource/NodeResource.java
+++ src/main/java/at/htl/indoornav/resource/NodeResource.java
@@ -5,6 +5,7 @@ import at.htl.indoornav.repository.NodeRepository;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -39,16 +40,14 @@ public class NodeResource {
 
     @POST
     @Path("/relationship")
-    public Response createRelationship(@QueryParam("start") String startName, @QueryParam("end") String endName) {
-        if (startName == null || endName == null) {
-            return Response.status(400).build();
-        }
-        Node nodeStart = nodeRepository.getNode(startName);
-        Node nodeEnd = nodeRepository.getNode(endName);
+    public Response createRelationship(@NotNull @QueryParam("start") String start, @NotNull @QueryParam("end") String end) {
+        Node nodeStart = nodeRepository.getNode(start);
+        Node nodeEnd = nodeRepository.getNode(end);
 
         if (nodeStart == null || nodeEnd == null) {
-            return Response.status(400).build();
+            return Response.status(404).build();
         }
+
         nodeRepository.createRelationship(nodeStart, nodeEnd);
         return Response.ok().build();
     }
